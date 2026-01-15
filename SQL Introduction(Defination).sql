483b1f139b908489057e87909c8be68cf59eee10
Sharzilkhan <sharzilkhan.ps12@gmail.com>
2026-01-15 17:11:06 +0530

    Add files via upload

---
+++ SQL Introduction(Defination).sql
@@ -0,0 +1,64 @@
+-------------------- SQL ------------------
+
+-- data : Raw facts and figure (information regarding anything)
+-- data types :  >structure(tables)  >un-structure(image,audio,video)  >semi - structure(application)
+ 
+ -- > database : to store structure format data or collection of table
+ -- . used to organise the data 
+ -- . store the data in digital format
+ -- . we can insert or update or delete the data
+ -- . there is no data duplicacy in digital format
+ 
+ 
+ -- > dbms(data base management system)
+ -- a software with which u can create , manage , store ,update , delete data
+ -- it is an intermidiate layer  between the user and your database 
+ 
+ -- [user] ----(dbms)----- > {database}
+ 
+ -- data integrity or data integration: linking data to anything
+
+-- > dbms types :
+-- .RDBMS (RELATIONAL DBMS)
+-- .NOSQL
+-- .HEIRACHARL
+-- .DOCUMENT
+
+-- > RDBMS (D/F company has d/f database)
+-- .            oracle  -->     MY SQL(comunity vesion of sql)
+-- .            Microsoft -->   SQL server
+-- .            postre sql
+-- .            maria DB
+-- . all database are same but slightly d/f from each other 
+
+
+-- . processing of SQL 
+
+-- .                    MY SQL(RDMS)
+--                           \/
+--              MY SQL WORKBENCH(IDE)(LIKE VS CODE)
+--                           \/
+--                        DATABASE
+--                           \/
+--                   TABLES(ROW AND COLUMN)
+--                           \/ 
+--                          DATA
+
+
+-- . WHAT IS SQL !
+-- . Structure query lang.
+-- . programing lang.
+-- . used to store , access ,delete.
+-- . worked as mediator.
+
+
+-- types of My Sql
+--                                       STATEMENT
+-- . DDL(DATA DEFINATION LANG)           CREATE , DROP , ALTER , TRUNCATE
+-- . DML(DATA MANIPULATE LANG)           INSERT , UPDATE , DELETE 
+-- . DCL(DATA CONTROL LANG)              GRANT , REVOKE
+-- . TCL(TRANSACTION CONTROL LANG)       ROLLBACK , COMMIT , SAVEPOINT
+-- . DQL(DATA QUERY LANG)                SELECT
+
+
+
